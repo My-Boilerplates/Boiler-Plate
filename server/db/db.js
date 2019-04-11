1152cf826eba088c6db569268ca49c72a1006226
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-10 22:30:46 -0500

    3rd commit

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,11 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://localhost:5432/yourdbname", {
-  logging: false // unless you like the logs
-  // ...and there are many other options you may want to play with
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432:yourdbname",
+  {
+    logging: false // unless you like the logs
+    // ...and there are many other options you may want to play with
+  }
+);
 
 module.exports = db;
